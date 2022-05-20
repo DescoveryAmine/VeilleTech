802c886f8cdbd3109277fd95008d99c9b2f14c8f
Djebbi Amine <aminedjebbie@gmail.com>
2022-05-20 15:16:00 +0200

    Adding restricts on routes

---
+++ frontend/src/routes/PostAuthRoutes.js
@@ -0,0 +1,60 @@
+import { lazy } from "react"
+
+const Home = lazy(() => import('../pages/Home.page'));
+const NewsFields = lazy(() => import('../pages/NewsFields.page'));
+const NewsArticles = lazy(() => import('../pages/NewsArticles.page'));
+const About = lazy(() => import('../pages/About.page'));
+const Contact = lazy(() => import('../pages/Contact.page'));
+const NotFound = lazy(() => import('../pages/Page404.page'));
+
+const routes = [
+    {
+        name: 'Home',
+        path: '/',
+        exact: true,
+        component: Home,
+        authentication: false,
+        theme: "theme-1"
+    },
+    {
+        name: 'News fields',
+        path: '/news/news-fields',
+        exact: true,
+        component: NewsFields,
+        authentication: false,
+        theme: "theme-1"
+    },
+    {
+        name: 'News Articles',
+        path: '/news/news-articles',
+        exact: true,
+        component: NewsArticles,
+        authentication: false,
+        theme: "theme-1"
+    },
+    {
+        name: 'About',
+        path: '/about',
+        exact: true,
+        component: About,
+        authentication: false,
+        theme: "theme-1"
+    },
+    {
+        name: 'Contact',
+        path: '/contact',
+        exact: true,
+        component: Contact,
+        authentication: false,
+        theme: "theme-1"
+    },
+    {
+        name: 'NotFound',
+        path: '/*',
+        exact: false,
+        component: NotFound,
+        authentication: false,
+    }
+];
+
+export default routes;
